8fdd86745eee4f6e82e93e6d8f249586f8745b45
Rekkyn <reedesler@gmail.com>
2013-04-01 00:10:41 -0700

    Cleaned up code

--- pressureplaids_common/rekkyn/pressureplaids/PressurePlaids.java
+++ pressureplaids_common/rekkyn/pressureplaids/PressurePlaids.java
@@ -18,29 +18,25 @@ import cpw.mods.fml.common.network.NetworkMod.SidedPacketHandler;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-
-@Mod(modid = PressurePlaids.modid, name = "Pressure Plaids", version = "-1.0")
+@Mod(modid = PressurePlaids.modid, name = "Pressure Plaids", version = "-0.5")
 @NetworkMod(
-        clientSideRequired = true, serverSideRequired = false,
-        clientPacketHandlerSpec = @SidedPacketHandler(
+        clientSideRequired = true, serverSideRequired = false, clientPacketHandlerSpec = @SidedPacketHandler(
                 channels = { "PressurePlaids" }, packetHandler = PlaidClientPacketHandler.class),
         serverPacketHandlerSpec = @SidedPacketHandler(
                 channels = { "PressurePlaids" }, packetHandler = PlaidServerPacketHandler.class))
-
 public class PressurePlaids {
     
     public static final String modid = "PressurePlaids";
     
     public static final Block pressurePlaid = new BlockPressurePlaid(500, EnumMobType.everything, Material.grass)
-    .setHardness(1F).setStepSound(Block.soundStoneFootstep)
-    .setUnlocalizedName("pressurePlaid").setCreativeTab(CreativeTabs.tabRedstone);
-
-    
+            .setHardness(1F).setStepSound(Block.soundStoneFootstep).setUnlocalizedName("pressurePlaid")
+            .setCreativeTab(CreativeTabs.tabRedstone);
     
     @Instance("PressurePlaids")
     public static PressurePlaids instance;
     
-    @SidedProxy(clientSide="rekkyn.pressureplaids.client.ClientProxy", serverSide="rekkyn.pressureplaids.CommonProxy")
+    @SidedProxy(
+            clientSide = "rekkyn.pressureplaids.client.ClientProxy", serverSide = "rekkyn.pressureplaids.CommonProxy")
     public static CommonProxy proxy;
     
     @PreInit
@@ -49,15 +45,15 @@ public class PressurePlaids {
     
     @Init
     public void load(FMLInitializationEvent event) {
-            proxy.registerRenderers();
-            
-            GameRegistry.registerBlock(pressurePlaid, "pressurePlaid");
-            LanguageRegistry.addName(pressurePlaid, "Pressure Plaid!");
-
+        proxy.registerRenderers();
+        
+        GameRegistry.registerBlock(pressurePlaid, "pressurePlaid");
+        LanguageRegistry.addName(pressurePlaid, "Pressure Plaid!");
+        
     }
     
     @PostInit
     public void postInit(FMLPostInitializationEvent event) {
     }
-
+    
 }
